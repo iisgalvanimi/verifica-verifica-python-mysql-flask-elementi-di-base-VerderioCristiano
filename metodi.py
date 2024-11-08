4911f837da66c9fdf3d8570f40af9bd1b9839d2b
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2024-11-08 11:46:36 +0000

    finito delete

--- metodi.py
+++ metodi.py
@@ -13,7 +13,7 @@ def connect_to_db():
     )
 
 # Rotta per ottenere il contenuto della tabella 'mammiferi' in formato JSON
-@app.route('/api/games', methods=['GET'])
+@app.route('/api/dati', methods=['GET'])
 def get_games():
     # Connessione al database
     mydb = connect_to_db()
@@ -29,6 +29,61 @@ def get_games():
     
     # Restituisce i dati in formato JSON
     return jsonify(games)
+
+@app.route('/api/dati', methods=['POST'])
+def add_game():
+    data = request.get_json()  # Recupera i dati JSON inviati dal client
+
+    # Verifica che tutti i campi necessari siano presenti
+    if not all(key in data for key in ('titolo', 'genere',  'piattaforma', 'data_uscita', 'sviluppatore')):
+        return jsonify({"error": "Dati mancanti"}), 400
+
+    titolo = data['titolo']
+    genere = data['genere']
+    piattaforma = data['piattaforma']
+    data_uscita = data['data_uscita']
+    sviluppatore = data['sviluppatore']
+
+    # Connessione al database e inserimento dei dati
+    mydb = connect_to_db()
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO games (titolo, genere, piattaforma, data_uscita, sviluppatore) VALUES (%s, %s, %s, %s, %s)"
+    values = (titolo, genere, piattaforma, data_uscita, sviluppatore)
+    
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+    mycursor.close()
+    mydb.close()
+
+    return jsonify({"message": "gioco inserito con successo!"}), 201
+
+
+
+@app.route('/api/dati/<int:id>', methods=['DELETE'])
+def delete_game(id):
+    mydb = connect_to_db()
+    mycursor = mydb.cursor()
+
+    # Verifica se il gioco con questo id esiste
+    mycursor.execute("SELECT * FROM games WHERE Id = %s", (id,))
+    if not mycursor.fetchone():
+        mycursor.close()
+        mydb.close()
+        return jsonify({"message": "gioco non trovato"}), 404
+
+    # Esegue l'operazione di DELETE
+    sql = "DELETE FROM games WHERE Id = %s"
+    mycursor.execute(sql, (id,))
+    mydb.commit()
+
+    mycursor.close()
+    mydb.close()
+
+    return jsonify({"message": "gioco eliminato con successo!"}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
